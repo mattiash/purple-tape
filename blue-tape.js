05e8d05fc98ec022f69d1cacc0c450013de2f2b6
Mattias Holmlund <mattias.git@holmlund.se>
2016-05-30 22:00:07 +0200

    Run tests with co

--- blue-tape.js
+++ blue-tape.js
@@ -1,47 +1,20 @@
 var Test = require('tape/lib/test')
-
-function checkPromise (p) {
-  return p && p.then && typeof p.then === 'function'
-}
+var co = require('co')
 
 Test.prototype.run = function () {
   if (this._skip) {
     return this.end()
   }
   this.emit('prerun')
-  try {
-    var p = this._cb && this._cb(this)
-    var isPromise = checkPromise(p)
-    var self = this
-    if (isPromise) {
-      p.then(function () {
-        self.end()
-      }, function (err) {
-        err ? self.error(err) : self.fail(err)
-        self.end()
-      })
-    }
-  } catch (err) {
-    if (err) {
-      this.error(err)
-    } else {
-      this.fail(err)
-    }
-    this.end()
-    return
-  }
-  this.emit('run')
-}
-
-Test.prototype.shouldFail = function (promise, clazz) {
-  return promise.then(function () {
-    throw new Error('should have failed')
+  var p = this._cb && co(this._cb(this))
+  var self = this
+  p.then(function () {
+    self.end()
   }, function (err) {
-    if (clazz && !(err instanceof clazz)) {
-      throw new Error('should have thrown an instance of ' + clazz)
-    }
-    this.ok(true)
-  }.bind(this))
+    err ? self.error(err) : self.fail(err)
+    self.end()
+  })
+  this.emit('run')
 }
 
 module.exports = require('tape')
